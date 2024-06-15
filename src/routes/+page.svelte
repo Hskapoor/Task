d24621c8b2eafa4cd51b2312f0ba2d833b270918
HARSIMRAN SINGH <54938803+Hskapoor@users.noreply.github.com>
2024-06-15 13:55:29 +0530

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,9 @@
 <script>
+	import { goto } from "$app/navigation";
 	import UserAuth from "../components/UserAuth.svelte";
 	import { logout } from "../stores/userStore";
 
 </script>
+<h4>Not Yet Registered ! Register Here</h4>
+<li><a href="/register" on:click|preventDefault={() => goto('/register')}>Register</a></li>
 <UserAuth></UserAuth>
